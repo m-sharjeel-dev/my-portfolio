8d87cda2f300f3a97f5109f802cc8abbcadaa8e4
m-sharjeel-dev <sharjeel.codes@gmail.com>
2025-10-24 23:31:40 +0500

    feat: add WhatsApp contact link to HeroSection and ContactSection

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -3,7 +3,7 @@
 import { personalData } from "@/utils/data/personal-data";
 import Image from "next/image";
 import Link from "next/link";
-import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { BsGithub, BsLinkedin, BsWhatsapp } from "react-icons/bs";
 import { FaFacebook, FaTwitterSquare } from "react-icons/fa";
 import { MdDownload } from "react-icons/md";
 import { RiContactsFill } from "react-icons/ri";
@@ -45,6 +45,13 @@ function HeroSection() {
               className="transition-all text-pink-500 hover:scale-125 duration-300"
             >
               <BsLinkedin size={30} />
+            </Link>
+            <Link
+              href={personalData.whatsapp}
+              target='_blank'
+              className="transition-all text-pink-500 hover:scale-125 duration-300"
+            >
+              <BsWhatsapp size={30} />
             </Link>
             {/* <Link
               href={personalData.facebook}
