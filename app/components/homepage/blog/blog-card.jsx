bcee4aab660691ff38707fb507c25ab780679e6b
sharjeelBitz <sharjeel.codes@gmail.com>
2026-07-20 01:31:31 +0500

    Update blog-card.jsx

--- app/components/homepage/blog/blog-card.jsx
+++ app/components/homepage/blog/blog-card.jsx
@@ -8,7 +8,7 @@ import { FaCommentAlt } from 'react-icons/fa';
 function BlogCard({ blog }) {
 
   return (
-    <div className="border border-[#1d293a] hover:border-[#464c6a] transition-all duration-500 bg-[#1b203e] rounded-lg relative group"
+    <div className="border border-[#1d293a] hover:border-[#464c6a] transition-all duration-500 bg-[#1b203e] rounded-lg relative group flex flex-col h-full"
     >
       <div className="h-44 lg:h-52 w-auto cursor-pointer overflow-hidden rounded-t-lg">
         <Image
@@ -19,7 +19,7 @@ function BlogCard({ blog }) {
           className='h-full w-full group-hover:scale-110 transition-all duration-300'
         />
       </div>
-      <div className="p-2 sm:p-3 flex flex-col flex-1 h-full">
+      <div className="p-2 sm:p-3 flex flex-col flex-1">
         <div className="flex justify-between items-center text-[#16f2b3] text-sm">
           <p>{timeConverter(blog.published_at)}</p>
         </div>
